3e4fcf4573e1b03b130c7b909b40f29410d1245e
MinhQuan <minh.quan040501@gmail.com>
2026-04-18 14:58:26 +0700

    Init

---
+++ src/main/java/com/ecomm/lunch_lunch/LunchLunchApplication.java
@@ -0,0 +1,13 @@
+package com.ecomm.lunch_lunch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchLunchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunchLunchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecomm/lunch_lunch/LunchLunchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomm.lunch_lunch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunchLunchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
